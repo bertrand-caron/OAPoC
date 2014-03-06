56cbc42ef61f3718cc87bbb6594385532f7ba288
Jimi van der Woning <jimivdwoning@gmail.com>
2014-03-06 14:54:03 +0100

    Fixed auto PDB download script

--- atompos/store_atb_pdb.py
+++ atompos/store_atb_pdb.py
@@ -5,7 +5,11 @@ import sys
 if __name__ == "__main__":
   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "atompos.settings")
 
-  from atompos.main.util import load_atb_pdb
+  from atompos.main.util import generate_atb_pdb, load_atb_pdb
 
   for atb_id in sys.argv[1:]:
-    load_atb_pdb(atb_id, True)
+    try:
+      load_atb_pdb(atb_id, True)
+    except:
+      generate_atb_pdb(atb_id)
+      load_atb_pdb(atb_id, True)
